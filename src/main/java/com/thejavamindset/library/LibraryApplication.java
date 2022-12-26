8d63f0a3b33bd1fee0285223abf281ce134988a7
The Java Mindset <thejavamindset@gmail.com>
2022-12-26 19:52:22 +0100

    02-configure-logback

--- src/main/java/com/thejavamindset/library/LibraryApplication.java
+++ src/main/java/com/thejavamindset/library/LibraryApplication.java
@@ -1,13 +1,17 @@
 package com.thejavamindset.library;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(LibraryApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
-		System.out.println("Hello World!");
+		log.info("Hello World!");
 	}
 }
