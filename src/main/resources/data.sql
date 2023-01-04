f46ecdcc6d1acd372ac749071b970a34ad6ebe40
The Java Mindset <thejavamindset@gmail.com>
2023-01-04 17:45:34 +0100

    05-add-bootstrap

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO Books (uuid, title, author, isbn) VALUES ('ed690645-ea72-4e77-b3c0-3190109ded22', 'Clean Code', 'Robert Cecil Martin', '9780132350884');
 INSERT INTO Books (uuid, title, author, isbn) VALUES ('363255f8-4fc6-4a4f-b4e4-4d07ac00903a', 'Effective Java', 'Joshua Bloch', '9780321356680');
+INSERT INTO Books (uuid, title, author, isbn) VALUES ('c434cc97-1907-4919-b95b-aafe3b7e4c8e', 'Java Concurrency in Practice', 'Brian Goetz', '9780321349606');
+INSERT INTO Books (uuid, title, author, isbn) VALUES ('9a66b99a-8b8a-11ed-a1eb-0242ac120002', 'Thinking in Java', 'Bruce Eckel', '9780131872486');
+INSERT INTO Books (uuid, title, author, isbn) VALUES ('a4d0fd6e-8b8a-11ed-a1eb-0242ac120002', 'Refactoring: Improving the Design of Existing Code', 'Martin Fowler', '9780134757599');
